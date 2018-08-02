7e7d1df7f3ddab3e2ef154c73c7a2aee7be8a60f
Webstir <bpgrandy@gmail.com>
2018-08-02 00:37:26 -0700

    pushy first push

---
+++ JS/global.js
@@ -0,0 +1,8 @@
+
+function includeJS(incFile)
+	{document.write('<script type="text/javascript" src="'+ incFile+ '"></script>');}
+
+includeJS('https://code.jquery.com/jquery-latest.js');
+
+includeJS('JS/pushy.js');
+
