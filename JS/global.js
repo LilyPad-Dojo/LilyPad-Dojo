4b20d6b0f9ae5cd0ac3c1434cefaf1ca69dbf17f
Webstir <bpgrandy@gmail.com>
2018-11-20 18:44:13 -0800

    change is a coming

--- JS/global.js
+++ JS/global.js
@@ -3,6 +3,3 @@ function includeJS(incFile)
 	{document.write('<script type="text/javascript" src="'+ incFile+ '"></script>');}
 
 includeJS('https://code.jquery.com/jquery-latest.js');
-
-includeJS('JS/pushy.js');
-
