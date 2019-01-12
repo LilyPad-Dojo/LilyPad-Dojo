e5512c285bc0ef76c36d4a81e1d5c861aa3d1fa7
Webstir <bpgrandy@gmail.com>
2019-01-12 00:12:14 -0800

    make more global

--- JS/global.js
+++ JS/global.js
@@ -3,3 +3,4 @@ function includeJS(incFile)
 	{document.write('<script type="text/javascript" src="'+ incFile+ '"></script>');}
 
 includeJS('https://code.jquery.com/jquery-latest.js');
+includeJS('../JS/pushy.js');
